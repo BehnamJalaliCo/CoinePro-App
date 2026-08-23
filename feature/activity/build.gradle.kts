fb8eed4affbe2ef55ae1e22939c2dc6d99156a49
Behnam Jalali <behnamjalalico@gmail.com>
2026-08-23 06:55:16 +0330

    feat: wire activity notification center

--- feature/activity/build.gradle.kts
+++ feature/activity/build.gradle.kts
@@ -15,7 +15,12 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:notifications"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:common"))
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
 }
