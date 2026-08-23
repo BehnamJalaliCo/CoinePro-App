2cd90580f582bcc3a0270adc1493fa2653b8e091
Behnam Jalali <behnamjalalico@gmail.com>
2026-08-23 14:33:45 +0330

    build(phase11): gate trader tools unit tests

--- feature/tools/build.gradle.kts
+++ feature/tools/build.gradle.kts
@@ -20,4 +20,5 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
 }
