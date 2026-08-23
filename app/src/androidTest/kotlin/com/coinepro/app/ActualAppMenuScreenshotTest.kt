8cffdcbc854fade958ba58ee67ff226643d6c68e
Behnam Jalali <behnamjalalico@gmail.com>
2026-08-24 02:17:44 +0330

    fix(phase17): compile actual app menu screenshot test

--- app/src/androidTest/kotlin/com/coinepro/app/ActualAppMenuScreenshotTest.kt
+++ app/src/androidTest/kotlin/com/coinepro/app/ActualAppMenuScreenshotTest.kt
@@ -1,12 +1,15 @@
 package com.coinepro.app
 
 import android.graphics.Bitmap
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.test.captureToImage
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -48,10 +51,8 @@ class ActualAppMenuScreenshotTest {
                             }
                         }
                     },
-                ) { padding ->
-                    androidx.compose.foundation.layout.Box(
-                        modifier = androidx.compose.ui.Modifier.padding(padding),
-                    ) {
+                ) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
                         HomeScreen(
                             state = MarketDataState(connection = MarketConnectionState.OFFLINE),
                             onRetry = {},
