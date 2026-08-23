7041c3ae0e4e4150152da9cd02c97a8514b75562
Behnam Jalali <behnamjalalico@gmail.com>
2026-08-23 13:47:37 +0330

    feat(phase10): add market intelligence core module

---
+++ core/marketintel/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.coinepro.core.marketintel"
+    compileSdk = 36
+    defaultConfig { minSdk = 26 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.kotlinx.coroutines.core)
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
